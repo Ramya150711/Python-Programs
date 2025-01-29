e501b32de7eb937fbe49c44fb10b88f7711d2507
K Ramya1 <karriramyap@gmail.com>
2025-01-29 10:58:53 +0530

    Add files via upload

---
+++ Untitled30.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import re
+def extract_emails(text):
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    emails = re.findall(email_pattern, text)
+    
+    return emails
+input_text = 'Contact us at support@example.com and sales@example.org.'
+emails = extract_emails(input_text)
+print(emails)
+
+
+# In[ ]:
+
+
+
+
